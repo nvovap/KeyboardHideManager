1c4402f3f29c239c7b27c9ca2f2af059ba486633
Vladimir Nevinniy <nvovap@gmail.com>
2017-10-22 21:24:26 +0300

    Initial Commit

---
+++ HideKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HideKeyboard
+//
+//  Created by Владимир Невинный on 22.10.17.
+//  Copyright © 2017 Владимир Невинный. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
